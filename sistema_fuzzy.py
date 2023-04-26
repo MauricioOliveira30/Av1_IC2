e6560f88a19abfc339c73e5d67f653ac72827d47
Maurício <66374666+MauricioOliveira30@users.noreply.github.com>
2023-04-25 23:33:49 -0300

    Update sistema_fuzzy.py

--- sistema_fuzzy.py
+++ sistema_fuzzy.py
@@ -47,7 +47,7 @@ while True:
             serviço = float(serviço)
     
 
-        if  (0 <= qualidade <= 10 and  0 <= serviço <= 10):
+        if  (0 > qualidade or qualidade> 10 and  0 > serviço or serviço> 10):
          print("Digite um valor entre 0 e 10:")
 
         else:
