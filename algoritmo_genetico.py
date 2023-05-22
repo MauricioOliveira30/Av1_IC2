682fe80d167d4b7f39fb30d9f49bd9760fd22e34
Maurício <66374666+MauricioOliveira30@users.noreply.github.com>
2023-05-22 12:58:06 +0000

    Protótipo de algoritmo_genetico

---
+++ algoritmo_genetico.py
@@ -0,0 +1,10 @@
+import random
+criptomoedas=['Bitcoin','Etherium','DodgeCoin','Ripple']
+preços_iniciais=[134143.65,9076.56,0.36,2.32]
+retorno_histórico= [[0.05, 0.03, 0.02, 0.01, 0.04],  
+    [0.01, 0.04, 0.05, 0.03, 0.02], 
+    [0.03, 0.01, 0.04, 0.02, 0.05]  
+]
+TAM_POPULAÇÃO=1000
+NUM_GERAÇÃO=100
+TAXA_MUTAÇÃO=0.001
